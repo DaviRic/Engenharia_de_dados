3d879f9e2eddbc696397316692a16547ea21f919
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-26 17:52:12 -0400

    [refactor] Alteração do banco de dados que será acessado pelo SQLite

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
@@ -2,17 +2,13 @@
 import sqlite3
 import pandas as pd # type: ignore
 
-con = sqlite3.connect('tutorial.db')
+con = sqlite3.connect(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\SQLite\Scripts\Teste.db')
 cursor = con.cursor()
 
 cursor.execute("INSERT INTO produto (id, nome, valor) VALUES (1, 'Tenis', 200.0)")
 
-con = sqlite3.connect('tutorial.db')
-
 query = 'SELECT * FROM produto'
 df = pd.read_sql(query, con)
-
-con = sqlite3.connect(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\SQLite\Scripts\Teste.db')
 
 con.commit()
 con.close()
