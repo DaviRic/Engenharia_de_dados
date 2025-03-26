8a5f964df630f1b699abc141169969c40ce30141
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-26 17:39:42 -0400

    [feat] Script Python que faz uma conexão com o SQLite

---
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
@@ -0,0 +1,28 @@
+
+import sqlite3
+import pandas as pd # type: ignore
+
+con = sqlite3.connect('tutorial.db')
+cursor = con.cursor()
+
+# cursor.execute("CREATE TABLE produto (id, nome, valor)")
+
+cursor.execute("INSERT INTO produto (id, nome, valor) VALUES (1, 'Tenis', 200.0)")
+
+con = sqlite3.connect('tutorial.db')
+
+query = 'SELECT * FROM produto'
+df = pd.read_sql(query, con)
+
+con.close()
+
+con = sqlite3.connect(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\SQLite\Scripts\Teste.db')
+cursor = con.cursor()
+# cursor.execute("CREATE TABLE produto (id, nome, valor)")
+
+# cursor.execute("INSERT INTO produto (id, nome, valor) VALUES (1, 'Tenis', 200.0)")
+
+con.commit()
+con.close()
+
+
