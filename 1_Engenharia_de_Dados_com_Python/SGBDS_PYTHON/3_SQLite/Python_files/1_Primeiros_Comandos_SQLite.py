09196d7700d5bd5a51f4493cbc5eb801cd504f1d
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-26 17:44:00 -0400

    [style] Remoção de linhas de código desnecessárias

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
@@ -5,16 +5,12 @@ import pandas as pd # type: ignore
 con = sqlite3.connect('tutorial.db')
 cursor = con.cursor()
 
-# cursor.execute("CREATE TABLE produto (id, nome, valor)")
-
 cursor.execute("INSERT INTO produto (id, nome, valor) VALUES (1, 'Tenis', 200.0)")
 
 con = sqlite3.connect('tutorial.db')
 
 query = 'SELECT * FROM produto'
 df = pd.read_sql(query, con)
-
-con.close()
 
 con = sqlite3.connect(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\SQLite\Scripts\Teste.db')
 cursor = con.cursor()
