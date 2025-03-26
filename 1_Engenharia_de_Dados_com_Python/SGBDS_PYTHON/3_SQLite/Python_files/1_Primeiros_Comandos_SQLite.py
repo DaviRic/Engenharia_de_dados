1931fc38c395cdf9e1e1b1d0e9e1e1ddbb765522
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-26 17:45:31 -0400

    [style] Limpeza de código. Remoção de comentários e linhas em branco

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/3_SQLite/Python_files/1_Primeiros_Comandos_SQLite.py
@@ -13,12 +13,6 @@ query = 'SELECT * FROM produto'
 df = pd.read_sql(query, con)
 
 con = sqlite3.connect(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\SQLite\Scripts\Teste.db')
-cursor = con.cursor()
-# cursor.execute("CREATE TABLE produto (id, nome, valor)")
-
-# cursor.execute("INSERT INTO produto (id, nome, valor) VALUES (1, 'Tenis', 200.0)")
 
 con.commit()
 con.close()
-
-
