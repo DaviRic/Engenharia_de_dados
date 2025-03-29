95ea74441fbbc106a34f7d405d209ddf0ac590d1
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-28 22:28:19 -0400

    [refactor] Remoção de linhas de exibição do dataframe usadas inicialmente no notebook

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/2_importando_csv.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/2_importando_csv.py
@@ -9,7 +9,6 @@ df_estado
 # Escolher estados específicos
 estados = ['Rio de Janeiro', 'Ceará', 'Rondônia', 'Bahia']
 df_estados = df.loc[df['state'].isin(estados)]
-df_estados.head(5)
 
 # Substituir os valores NaN. Esse valor quebra na leitura do dataframe
 df['street'] = df['street'].fillna('Não registrado')
@@ -20,24 +19,17 @@ df['state'] = df['state'].fillna('Estado não informado')
 
 df['state'] = df['state'].replace('São Paulo', 'Sao Paulo')
 
-
 # Usando variáveis como parâmetro no método replace
 valor_atual = 'Amazonas'
 valor_substituto = 'AM'
 
 df['state'] = df['state'].replace(valor_atual, valor_substituto)
-df.head(3)
 
 # Fazendo a substituição de vários valores ao mesmo tempo
 df_lista_estados = {'Paraná': 'PR', 'Rondônia': 'RO', 'Pará': 'PA', 'Minas Gerais': 'MG'}
 df['state'] = df['state'].replace(df_lista_estados)
 
-
 debug_caminho = r"C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Caquinha.xlsx"
 
 df_debug = pd.read_excel(debug_caminho)
 df_debug.head()
-
-
-
-
