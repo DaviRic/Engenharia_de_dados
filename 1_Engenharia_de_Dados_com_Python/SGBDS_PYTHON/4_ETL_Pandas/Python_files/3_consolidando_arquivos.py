324c65743226dd78f8f9c750c138f4ab508bf181
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-30 22:41:19 -0400

    [feat] Algoritmo em Python que faz a consolidação dos dados para se fazer o ETL

---
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/3_consolidando_arquivos.py
@@ -0,0 +1,38 @@
+import pandas as pd # type: ignore
+import os
+
+
+vendas_2021 = pd.read_excel(r"")
+vendas_2022 = pd.read_excel(r"")
+vendas_2023 = pd.read_excel(r"")
+
+diretorio = r"C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivo_leitura_varios"
+
+# Variável que vai armazenar os dados dos arquivos .xlsx
+dados = []
+
+# Itera sobre os arquivos .xlsx presentes no diretório
+for arquivo in os.listdir(diretorio):
+    if arquivo.endswith('.xlsx'):
+        caminho_arquivo = os.path.join(diretorio, arquivo)
+        df = pd.read_excel(caminho_arquivo)
+        dados.append(df)
+
+consolidado = pd.concat(dados)
+consolidado.head()
+
+# #### Salvando arquivo consolidado em uma pasta específica
+
+
+pasta_destino = r"C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivo_consolidado\\"
+
+nome_arquivo = "union_files.xlsx"
+
+caminho_completo = pasta_destino + nome_arquivo
+
+consolidado.to_excel(caminho_completo, index=False)
+
+
+
+
+
