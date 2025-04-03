6bd23464405fc586608df2261934bfe59631c1ce
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-03 17:50:52 -0400

    [style] Remoção de alguns prints e linhas em branco

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
@@ -7,36 +7,17 @@ df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS
 for coluna in df.columns:
     print(f'Coluna {coluna} é do tipo: {df[coluna].dtype}')
 
-
 for coluna in df.columns:
     print(f'Colunha {coluna} tem é do tipo {df[coluna].dtypes}')
 
-
 df['id'] = df['id'].astype(float)
-
-
-print(df['id'].dtype)
-
 
 df['number'] = df['number'].astype(str)
 
-
-print(df['number'].dtype)
-
-
-# #### Conversão de Datas
-
-
 df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
-df
-
-
-print(df['created_at'].dtypes)
-
 
 # Convertendo a colunas created_at para o formato data e hora
 df['created_at'] = pd.to_datetime(df['created_at'])
-
 
 # Filtrando um intervalo de tempo e passando para um novo dataframe
 df_filtrado = df[(df['created_at'].dt.year >= 2018) & (df['created_at'].dt.year <= 2020)]
@@ -53,9 +34,6 @@ df_janeiro_2018['created_at'] = df_janeiro_2018['created_at'].dt.strftime('%d/%m
 
 
 df_janeiro_2018['created_at'].dtype
-
-
-# #### Converter para o padrão BR no primeiro comando
 
 
 df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
