6649a5f947355304d6ddbf13898bd8d20b93da04
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-03 17:52:24 -0400

    [style] Limpeza de código. Remoção de prints e linhas em branco

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
@@ -1,8 +1,6 @@
 import pandas as pd # type: ignore
 
-
 df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
-
 
 for coluna in df.columns:
     print(f'Coluna {coluna} é do tipo: {df[coluna].dtype}')
@@ -22,19 +20,13 @@ df['created_at'] = pd.to_datetime(df['created_at'])
 # Filtrando um intervalo de tempo e passando para um novo dataframe
 df_filtrado = df[(df['created_at'].dt.year >= 2018) & (df['created_at'].dt.year <= 2020)]
 
-
-
 # Filtrando somente as datas de janeiro de 2018
 df_janeiro_2018 = df[(df['created_at'].dt.year == 2018) & (df['created_at'].dt.month == 1)]
-df_janeiro_2018.head(3)
-
 
 # Converter a coluna 'created_at' para o formato padrão brasileiro
 df_janeiro_2018['created_at'] = df_janeiro_2018['created_at'].dt.strftime('%d/%m/%Y')
 
-
 df_janeiro_2018['created_at'].dtype
-
 
 df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
 
@@ -43,9 +35,3 @@ df['created_at'] = pd.to_datetime(df['created_at'])
 
 # Transformando a data para o padrão brasileiro
 df['created_at'] = df['created_at'].dt.strftime('%d/%m/%Y %H:%M:%S')
-df
-
-
-
-
-
