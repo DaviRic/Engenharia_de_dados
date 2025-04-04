25b46faed5aade0b50eac450a247d839bcf315f1
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-03 20:56:01 -0400

    [refactor] Encapsualando em uma função o script de tipagem das colunas

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/6_conversao_dados.py
@@ -1,37 +1,29 @@
 import pandas as pd # type: ignore
+def cast_types():
+    df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
 
-df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
+    df['id'] = df['id'].astype(float)
 
-for coluna in df.columns:
-    print(f'Coluna {coluna} é do tipo: {df[coluna].dtype}')
+    df['number'] = df['number'].astype(str)
 
-for coluna in df.columns:
-    print(f'Colunha {coluna} tem é do tipo {df[coluna].dtypes}')
+    # Convertendo a colunas created_at para o formato data e hora
+    df['created_at'] = pd.to_datetime(df['created_at'])
 
-df['id'] = df['id'].astype(float)
+    # Filtrando um intervalo de tempo e passando para um novo dataframe
+    df_filtrado = df[(df['created_at'].dt.year >= 2018) & (df['created_at'].dt.year <= 2020)]
 
-df['number'] = df['number'].astype(str)
+    # Filtrando somente as datas de janeiro de 2018
+    df_janeiro_2018 = df[(df['created_at'].dt.year == 2018) & (df['created_at'].dt.month == 1)]
 
-df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
+    # Converter a coluna 'created_at' para o formato padrão brasileiro
+    df_janeiro_2018['created_at'] = df_janeiro_2018['created_at'].dt.strftime('%d/%m/%Y')
 
-# Convertendo a colunas created_at para o formato data e hora
-df['created_at'] = pd.to_datetime(df['created_at'])
+    df_janeiro_2018['created_at'].dtype
 
-# Filtrando um intervalo de tempo e passando para um novo dataframe
-df_filtrado = df[(df['created_at'].dt.year >= 2018) & (df['created_at'].dt.year <= 2020)]
+    # Transformando a coluna created_at em formato de data
+    df['created_at'] = pd.to_datetime(df['created_at'])
 
-# Filtrando somente as datas de janeiro de 2018
-df_janeiro_2018 = df[(df['created_at'].dt.year == 2018) & (df['created_at'].dt.month == 1)]
+    # Transformando a data para o padrão brasileiro
+    df['created_at'] = df['created_at'].dt.strftime('%d/%m/%Y %H:%M:%S')
 
-# Converter a coluna 'created_at' para o formato padrão brasileiro
-df_janeiro_2018['created_at'] = df_janeiro_2018['created_at'].dt.strftime('%d/%m/%Y')
-
-df_janeiro_2018['created_at'].dtype
-
-df = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
-
-# Transformando a coluna created_at em formato de data
-df['created_at'] = pd.to_datetime(df['created_at'])
-
-# Transformando a data para o padrão brasileiro
-df['created_at'] = df['created_at'].dt.strftime('%d/%m/%Y %H:%M:%S')
+cast_types()
