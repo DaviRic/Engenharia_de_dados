9cf01d6abd670af5673d7a79546f934cd753efb0
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-11 18:56:00 -0400

    [feat] Script em Python para tratar duplicadas de DataFrames

---
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/10_removendo_duplicatas.py
@@ -0,0 +1,32 @@
+import pandas as pd # type: ignore
+
+df = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto_dup.xlsx')
+df.head(10)
+
+df.duplicated()
+
+df.duplicated(['Name'])
+
+# Armazena as linhas duplicadas em um dataframe o que melhora significativamente a vizualização dos dados
+duplicados = df[df.duplicated()]
+duplicados.head()
+
+# Removendo as duplicatas e armazenando em uma variável o DF sem nenhuma duplicata
+df_sem_duplicatas = df.drop_duplicates()
+
+''' Nesse ponto o dataframe está tratado e eu posso
+levar o documento para o destino que eu queira (Banco de dados
+                                                arquivos excel,
+                                                txt, etc)'''
+df_sem_duplicatas
+
+df_sem_dup_parametro = df.drop_duplicates(subset=['ID'])
+df_sem_dup_parametro
+
+# Removendo diretamente as duplicatas do DF (inplace). Com esse método não é necessário criar um outro DataFrame
+# Ele remove as duplicatas diretamente no dataframe
+df.drop_duplicates(inplace=True)
+
+
+
+
