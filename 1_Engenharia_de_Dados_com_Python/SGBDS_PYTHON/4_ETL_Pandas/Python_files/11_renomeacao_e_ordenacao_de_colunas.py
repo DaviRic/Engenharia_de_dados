cd1d21490d61b64271ca14e9ed54139a369f4f22
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-20 23:14:48 -0400

    [style] Removendo algumas linhas de visualização, como 'print' ou 'display'

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/11_renomeacao_e_ordenacao_de_colunas.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/11_renomeacao_e_ordenacao_de_colunas.py
@@ -1,12 +1,9 @@
-import pandas as pd
+import pandas as pd # type: ignore
 
 dados = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto.xlsx')
-dados.head(3)
 
 # Ordenar em ordem crescente linhas a partir de uma coluna
 dados = dados.sort_values(by=['Name'])
-dados.head(10)
 
 # O parâmetro ascending serve para indicar se é para ordenar de forma crescente ou decrescente
 dados_decrescente = dados.sort_values(by=['ID'], ascending=False)
-dados_decrescente.head()
