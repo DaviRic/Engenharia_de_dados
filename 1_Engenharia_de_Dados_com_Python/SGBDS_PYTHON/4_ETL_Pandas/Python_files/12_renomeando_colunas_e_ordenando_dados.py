6bee27ea2ebc625d0f3baf44dc6dd2d5d6b0c500
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-24 16:09:30 -0400

    [refactor] Encapsulando em uma função o script que renomeia colunas de um dataframe e ordena seus dados

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/12_renomeando_colunas_e_ordenando_dados.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/12_renomeando_colunas_e_ordenando_dados.py
@@ -1,14 +1,16 @@
 import pandas as pd # type: ignore
 
-dados = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto.xlsx')
+def rename_reorder_data(dados):
 
-# Renomeando as colunas do DataFrame
-dados = dados.rename(columns={'Name':'Full_Name'})
+    # Renomeando as colunas do DataFrame
+    dados = dados.rename(columns={'Name':'Full_Name'})
 
-# Renomeando as colunas usando variável
-colunas = {'Full_Name':'Name', 'ID':'Id'}
-dados = dados.rename(columns=colunas)
+    # Renomeando as colunas usando variável
+    colunas = {'Full_Name':'Name', 'ID':'Id'}
+    dados = dados.rename(columns=colunas)
+    return dados
 
-
+dados = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto.xlsx') 
+dataframe = rename_reorder_data(dados)
 
 
