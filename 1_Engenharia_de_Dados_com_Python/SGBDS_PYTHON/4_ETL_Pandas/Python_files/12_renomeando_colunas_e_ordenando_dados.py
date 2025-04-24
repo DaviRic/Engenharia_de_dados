d0473c16d988c7982488ed2682d2b6d3c3070a21
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-24 15:56:56 -0400

    [style] Remoção de comentário desnecessários

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/12_renomeando_colunas_e_ordenando_dados.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/12_renomeando_colunas_e_ordenando_dados.py
@@ -1,21 +1,16 @@
-# %%
-import pandas as pd
+import pandas as pd # type: ignore
 
-# %%
 dados = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto.xlsx')
 
-# %%
 # Renomeando as colunas do DataFrame
 dados = dados.rename(columns={'Name':'Full_Name'})
 dados.head(1)
 
-# %%
 # Renomeando as colunas usando variável
 colunas = {'Full_Name':'Name', 'ID':'Id'}
 dados = dados.rename(columns=colunas)
 dados.head(1)
 
-# %%
 
 
 
