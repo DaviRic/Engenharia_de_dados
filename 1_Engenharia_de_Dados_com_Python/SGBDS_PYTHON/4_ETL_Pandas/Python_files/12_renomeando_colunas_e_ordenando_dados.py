955c218fc210e4f07263eaabda9608e5e5958d03
DaviCruvel <davi.r.cruvel@gmail.com>
2025-04-24 15:55:26 -0400

    [feat] Script usado para renomear colunas de um dataframe e ordenar dados

---
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/4_ETL_Pandas/Python_files/12_renomeando_colunas_e_ordenando_dados.py
@@ -0,0 +1,21 @@
+# %%
+import pandas as pd
+
+# %%
+dados = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Produto.xlsx')
+
+# %%
+# Renomeando as colunas do DataFrame
+dados = dados.rename(columns={'Name':'Full_Name'})
+dados.head(1)
+
+# %%
+# Renomeando as colunas usando variável
+colunas = {'Full_Name':'Name', 'ID':'Id'}
+dados = dados.rename(columns=colunas)
+dados.head(1)
+
+# %%
+
+
+
