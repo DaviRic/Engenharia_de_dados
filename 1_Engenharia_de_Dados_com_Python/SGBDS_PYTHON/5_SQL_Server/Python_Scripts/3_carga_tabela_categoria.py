876212d1cb7c28c203c598118909bdb765a75537
DaviCruvel <davi.r.cruvel@gmail.com>
2025-05-11 21:17:40 -0400

    [style] Removendo algumas linha de códigos desnecessárias

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/3_carga_tabela_categoria.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/3_carga_tabela_categoria.py
@@ -7,12 +7,10 @@ conexaoDB = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'f'SERVER={se
 cursor = conexaoDB.cursor() # criando o cursor de comando para executar as consultas (queries)
 
 dados_categoria = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Categoria.xlsx')
-# dados_categoria.head()
 
 cursor.execute('DELETE FROM [Categoria]')
 cursor.commit()
 
-dados_categoria.head(5)
 
 for index, linha in dados_categoria.iterrows():
     # Insere os dados nas colunas
