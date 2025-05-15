a28de4827b8c21aa293999f1277bdefabe5cb586
DaviCruvel <davi.r.cruvel@gmail.com>
2025-05-14 21:58:02 -0400

    [style] Remoção de comentários e espaços do código

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/4_carga_tabela_itens.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/4_carga_tabela_itens.py
@@ -9,13 +9,11 @@ cursor = conexaoDB.cursor() # criando o cursor de comando para executar as consu
 
 
 dados_itens = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\items.xlsx')
-# dados_itens.head()
 
 cursor.execute('DELETE FROM [Itens]')
 cursor.commit()
 
 dados_itens['total_price'] = dados_itens['total_price'].astype(float)
-# dados_itens.head(5)
 
 for index, linha in dados_itens.iterrows():
     # Insere os dados nas colunas
@@ -24,5 +22,3 @@ for index, linha in dados_itens.iterrows():
 cursor.commit()     # Valida os dados no SQL Server
 cursor.close()      # Fecha o cursor
 conexaoDB.close()   # Fecha a conexão do SQL Server
-
-
