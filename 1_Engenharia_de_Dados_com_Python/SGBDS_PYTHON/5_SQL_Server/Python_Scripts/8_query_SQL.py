fd120637cdd42c37f662ff13207108ea6fa355db
DaviCruvel <davi.r.cruvel@gmail.com>
2025-05-28 18:07:47 -0400

    [refactor] Removendo algumas linhas para deixar o código e a execução mais limpa

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/8_query_SQL.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/8_query_SQL.py
@@ -7,18 +7,15 @@ conexaoDB = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'f'SERVER={se
 cursor = conexaoDB.cursor() # criando o cursor de comando para executar as consultas (queries)
 
 categoria = pd.read_sql("SELECT * FROM Categoria", conexaoDB)
-categoria.head(10)
 
 query = 'SELECT * FROM Categoria'
 categoria_df = pd.read_sql(query, conexaoDB)
-categoria_df.head(10)
 
 query_clientes = """SELECT * FROM [dbo].[Clientes]
                     WHERE state_brazil in ('Acre', 'Mato Grosso', 'Goiás', 'Maranhão')
                     AND first_name in ('Stephanie', 'Laura', 'Heitor', 'Bruno')"""
 
 clientes_especificos = pd.read_sql(query_clientes, conexaoDB)
-clientes_especificos.head(10)
 
 query_ordens = """SELECT * FROM [dbo].[Ordens]"""
 
