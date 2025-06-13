14ad1f563af1ce8221db68e4fb34e5e28cccf4de
DaviCruvel <davi.r.cruvel@gmail.com>
2025-06-13 17:42:13 -0400

    [refactor] Limpando linhas de códigos desnecessário no script

--- 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/10_merge_juncao_de_tabelas.py
+++ 1_Engenharia_de_Dados_com_Python/SGBDS_PYTHON/5_SQL_Server/Python_Scripts/10_merge_juncao_de_tabelas.py
@@ -12,8 +12,6 @@ Itens = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\
 Ordens = pd.read_excel(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_excel\Ordens.xlsx')
 Clientes = pd.read_csv(r'C:\Users\Cliente\Desktop\Engenharia_de_dados_com_Python\SGBDS_PYTHON\4_ETL Pandas\arquivos_csv\Clientes.csv', delimiter=',')
 
-Ordens.head(1)
-
 Clientes.head(1)
 
 T1 = pd.merge(Ordens, Clientes, left_on="customer_id", right_on="id", how="left")
@@ -23,17 +21,12 @@ T1 = pd.merge(Ordens, Clientes, left_on="customer_id", right_on="id", how="left"
 # Inner -> retorna os dados apenas quando as duas tabelas tem chaves correspondentes
 # Outer -> retorna todos os registros das duas tabelas
 
-T1.head(1)
-
 T1 = T1[["id_x", "created_at_x", "first_name", "cell_phone", "state"]]
-T1.head(1)
 
 T2 = pd.merge(Itens, Produtos, left_on="product_id", right_on="ID", how="left")
-T2.head(1)
 
 # Escolhi só as colunas que me importam
 T2 = T2[["id", "Name", "Id_Category"]]
-T2.head(3)
 
 categoria_prod = pd.merge(T2, Categoria, left_on="Id_Category", right_on="id", how="left")
 categoria_prod.head(5)
